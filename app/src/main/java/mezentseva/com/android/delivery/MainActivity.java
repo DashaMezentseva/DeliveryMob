2ed53878ec220d2f3f8d2243c7bd3a190f6f54bc
Dasha Mezentseva <dashamezen9988@gmail.com>
2018-04-29 14:01:21 +0300

    Initial commit

---
+++ app/src/main/java/mezentseva/com/android/delivery/MainActivity.java
@@ -0,0 +1,43 @@
+package mezentseva.com.android.delivery;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignUp, btnSignIn;
+    TextView textSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        textSlogan = (TextView)findViewById(R.id.textSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Nabila.ttf");
+        textSlogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+}
