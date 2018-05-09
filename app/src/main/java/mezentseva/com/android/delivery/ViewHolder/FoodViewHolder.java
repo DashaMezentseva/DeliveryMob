904a2b585776d5ad0de3391ad7e467c82793be72
Dasha Mezentseva <dashamezen9988@gmail.com>
2018-05-09 22:29:31 +0300

    List of Food on the menu

---
+++ app/src/main/java/mezentseva/com/android/delivery/ViewHolder/FoodViewHolder.java
@@ -0,0 +1,38 @@
+package mezentseva.com.android.delivery.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import mezentseva.com.android.delivery.Interface.ItemClickListener;
+import mezentseva.com.android.delivery.R;
+
+public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView food_name;
+    public ImageView food_image;
+
+    private ItemClickListener itemClickListener;
+
+
+
+    public FoodViewHolder(View itemView) {
+        super(itemView);
+
+        food_name = (TextView)itemView.findViewById(R.id.food_name);
+        food_image = (ImageView)itemView.findViewById(R.id.food_image);
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onClick(v,getAdapterPosition(),false);
+
+    }
+}
